fc4dbf0a899e2485fe248a01ad0d699c4a097869
Arne Kiesewetter <banane9@live.de>
2024-02-12 17:21:42 +0100

    Add various performance tracking

--- MonkeyLoader.Unity.Integration/UnityHooks.cs
+++ MonkeyLoader.Unity.Integration/UnityHooks.cs
@@ -2,16 +2,18 @@
 using MonkeyLoader.Patching;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace MonkeyLoader.Unity
 {
-    internal sealed class SceneLoadingHook : Monkey<SceneLoadingHook>
+    internal sealed class UnityHooks : Monkey<UnityHooks>
     {
-        public override string Name { get; } = "Scene Loading Hook";
+        public override string Name { get; } = "Unity Hooks";
 
         private static IEnumerable<IUnityMonkeyInternal> UnityMonkeys
             => Mod.Loader.Monkeys.SelectCastable<IMonkey, IUnityMonkeyInternal>();
@@ -21,17 +23,26 @@ namespace MonkeyLoader.Unity
         protected override bool OnLoaded()
         {
             SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+            Application.quitting += () => Mod.Loader.Shutdown();
+
             return true;
         }
 
         private void SceneManager_sceneLoaded(Scene scene, LoadSceneMode sceneMode)
         {
-            Info(() => "First Scene Loaded! Calling OnFirstSceneReady on UnityMonkeys!");
+            Info(() => "First Scene Loaded! Executing OnFirstSceneReady hooks on UnityMonkeys!");
+
+            var unityMonkeys = UnityMonkeys.ToArray();
+            Logger.Trace(() => "Running FirstSceneReady hooks in this order:");
+            Logger.Trace(unityMonkeys.Select(uM => new Func<object>(() => $"{uM.Mod.Title}/{uM.Name}")));
 
             SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+            var sw = Stopwatch.StartNew();
 
             foreach (var unityMonkey in UnityMonkeys)
                 unityMonkey.FirstSceneReady(scene);
+
+            Info(() => $"Done executing OnFirstSceneReady hooks on UnityMonkeys in {sw.ElapsedMilliseconds}ms!");
         }
     }
 }
